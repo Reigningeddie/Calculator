ffe41811bd017ee51190bf2325d315f08a6ed7e9
Eduardo Aguilar <eaguilar92105@yahoo.com>
2022-12-12 15:22:53 -0800

    got input elements to display into output

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const keys = document.querySelector('.pad');
+const output = document.querySelector('.output');
 
 keys.addEventListener('click', function(e) {
-  const value = event.target.value
-  console.log(value)
+  const input = event.target.value
+  console.log(input)
+
+  const display = output.value
+  console.log(display)
+
+
+  if (display === 'Infinity') {
+    output.value = input
+  } else {
+    output.value = display + input
+  }
+
 })
