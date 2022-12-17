48bd5a692963a77a78acf842738e80f9c3492a18
Eduardo Aguilar <eaguilar92105@yahoo.com>
2022-12-16 21:20:31 -0800

    check if current display is zero and then take in input

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const calculator = {
 function input(num) {
   const { display } = calculator;
 
-  calculator.display = display === '0' ? num : display + num
+  calculator.display = display == '0' ? num : display + num
 }
 
 keys.addEventListener('click', e => {
@@ -48,56 +48,3 @@ function Display() {
 }
 
 Display();
-
-
-
-
-// const keys = document.querySelector('.pad');
-// const output = document.querySelector('.output');
-// const display = output.value
-
-
-
-
-// keys.addEventListener('click', function(e) {
-//   const pad = event.target
-//   const input = event.target.value
-//   const display = output.value
-
-//   if (display.length === 10) {
-//     const erase = () => {
-//       output.value = '0'
-//     }
-//     alert('ERROR: Maximum amount of characters reached.')
-//     erase();
-//   }
-
-//   if (pad.classList.contains('clear')) {
-//     output.value = '0'
-//   }
-  
-//   if (pad.classList.contains('equal')) {
-//     let solve = (eval(display))
-//     output.value = solve
-//   }
-
-//   if (pad.classList.contains('num')) {
-//     if (display === '0') {
-//       output.value = input
-//     } else {
-//       output.value = display + input
-//     }
-//   }
-
-//   if (pad.classList.contains('operators')) {
-//     if (display.includes('+') || display.includes('-') || display.includes('*') || display.includes('/')) {
-//       return alert('ERROR: One operator per expression.')
-//   } else if (display === '0') {
-//     return '0'
-//   }   else {
-//     output.value = display + input
-//     }
-//   }
-
-// })
-
